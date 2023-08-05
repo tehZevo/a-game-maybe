31f688bf4a934dc64f708d231dfa31ff3158def9
tehZevo <tehzevo@users.noreply.github.com>
2023-08-04 20:57:37 -0400

    Add gitignore

--- entity.py
+++ entity.py
@@ -1,9 +1,11 @@
 import pygame
 
+ENTITY_SIZE = 32
+
 class Entity(pygame.sprite.Sprite):
   def __init__(self):
     super().__init__()
-    self.surf = pygame.Surface((75, 25))
+    self.surf = pygame.Surface((ENTITY_SIZE, ENTITY_SIZE))
     self.surf.fill((255, 255, 255))
     self.rect = self.surf.get_rect()
 
@@ -12,4 +14,4 @@ class Entity(pygame.sprite.Sprite):
     self.rect.center = [x + dx, y + dy]
 
   def draw(self, screen):
-    screen.blit(self.surf, self.rect)
+    screen.blit(self.surf, self.rect.center)
