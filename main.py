e24c749b1d3fe3da6bff14a61be50ec0059efa38
tehZevo <tehzevo@users.noreply.github.com>
2023-08-08 00:14:01 -0400

    Update todos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from game import Game
 
+#TODO: batch sprites more (use groups)
 #TODO: top level skill wrapper that defines things like use delay
 #TODO: ECS for particles so i can separate out particle behaviors
 #TODO: collisions
@@ -7,13 +8,8 @@ from game import Game
 #TODO: fix subpixel glitchiness
 #TODO: make 0, 0 center of sprites
 #TODO: box camera
-#TODO: use pygame vector https://www.pygame.org/docs/ref/math.html#pygame.math.Vector2
 #TODO: add ysorting to camera
-#TODO: make interactible interface for components
-#TODO: need to create new player anyway... that way he has a reference to the new world
 #TODO: generator should make a player spawn point component, which players are spawned on by the generators spawn method
-#TODO: use movespeed from stats
-#TODO: persist player data across floors
 #TODO: gold, health, mana drops: walk over them to pick them up
 #TODO: maybe distinction between world sprite and ui sprite
 #TODO: skill idea: "ally bomb" (or something like that): damage enemies nearby allies (target allies, then target enemies)
